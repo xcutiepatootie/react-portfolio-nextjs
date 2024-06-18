7c8774be1b404882d6359ba1841125e1762e58f8
xcutiepatootie <mackycalabia@gmail.com>
2024-06-18 13:34:59 +0800

    refactor: positioning and hiding scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import styles from "@/lib/CSSModules/HideScrollbar.module.css";
 
 export default function Home() {
-  return <div className="flex items-center justify-center">Test</div>;
+  return (
+    <div className="flex items-center justify-center h-screen overflow-y-hidden">
+      Test
+    </div>
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
+const links = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "/",
+  },
+  {
+    name: "Projects",
+    link: "/",
+  },
+  {
+    name: "Contact",
+    link: "/",
+  },
+];
+
 const Header = () => {
-  return <div className="h-24">Header</div>;
+  return (
+    <div className="sticky top-0 left-0 z-50 h-24 bg-inherit">
+      <div className="flex justify-between">
+        <span>Logo Here</span>
+        {links.map((link, index) => {
+          return <h1 key={index}>{link.name}</h1>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
