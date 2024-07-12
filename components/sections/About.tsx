d61c62ef44495ec2cc75f9aacad84947735c6053
xcutiepatootie <mackycalabia@gmail.com>
2024-07-12 23:29:25 +0800

    feat: added card for technical skills

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      About
+      <div className="flex h-24 w-24 items-center justify-center rounded-xl drop-shadow-2xl shadow-xl bg-slate-100">
+        Technical Skills
+      </div>
+    </div>
+  );
 };
 
 export default About;
