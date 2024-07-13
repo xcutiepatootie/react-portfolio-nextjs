802096cd3aff7f5b3c584b6d86fcc87d125823ca
xcutiepatootie <mackycalabia@gmail.com>
2024-07-13 10:59:44 +0800

    feat: added custom fonts for future purpose

---
+++ utils/fonts.ts
@@ -0,0 +1,12 @@
+import { Roboto_Condensed } from "next/font/google";
+
+const Roboto_Condensed_init = Roboto_Condensed({
+  weight: "400",
+  style: "normal",
+  preload: false,
+});
+
+
+
+export const roboto_condensed = Roboto_Condensed_init;
+
