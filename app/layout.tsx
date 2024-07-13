39eff765930a5dfddad3bc31345004ffdad22ab4
xcutiepatootie <mackycalabia@gmail.com>
2024-07-13 10:59:54 +0800

    feat: changed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { FramerMotionProvider } from "@/utils/providers/FramerMotionProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto_condensed = Roboto_Condensed({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={`h-screen ${inter.className}`}>
+      <body className={`h-screen ${roboto_condensed.className}`}>
         <div className="flex h-full flex-col">
           <Header />
           <main className="flex-grow overflow-hidden">
