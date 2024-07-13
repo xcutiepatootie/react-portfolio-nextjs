dda3cca821db60f85e71808ab50b2f69c6ef51bc
xcutiepatootie <mackycalabia@gmail.com>
2024-07-13 12:24:58 +0800

    feat: fill the data of the cards

--- components/Cards_Data.tsx
+++ components/Cards_Data.tsx
@@ -1,53 +1,16 @@
 import React, { ReactNode } from "react";
 import { TypeAnimation } from "react-type-animation";
 
-type HomeCard = {
+type AboutCard = {
   id: number;
+  title: string;
   content: ReactNode;
 };
 
-export const home_cards: HomeCard[] = [
-  {
-    id: 0,
-    content: (
-      <>
-        <div className="flex flex-col items-center justify-center">
-          <div className="flex h-full w-96 items-center justify-center p-4 bg-blend-color-burn">
-            <div className="flex flex-col items-center justify-center">
-              <h1>Calabia, Mackrislan A. &quot;Macky&quot;</h1>
-              <TypeAnimation
-                className="text-center text-2xl"
-                sequence={[
-                  "I am a Web Developer.",
-                  1000,
-                  `I am a Certified MikroTik Network Associate.`,
-                  1000,
-                  "I am a Programmer.",
-                  1000,
-                  `I enjoy learning new things and technologies.`,
-                  1000,
-                ]}
-                wrapper="span"
-                speed={80}
-                deletionSpeed={50}
-                style={{
-                  whiteSpace: "pre-line",
-                }}
-                repeat={Infinity}
-              />
-              <h4>and a</h4>
-              <h2>Computer Science Graduate</h2>
-            </div>
-          </div>
-        </div>
-      </>
-    ),
-  },
-  { id: 1, content: "E" },
-  { id: 2, content: "S" },
-  { id: 3, content: "T" },
+export const about_cards: AboutCard[] = [
+  { id: 0, title: "Technical Skills", content: <>Test1</> },
+  { id: 1, title: "Soft Skills", content: <>Test2</> },
+  { id: 2, title: "Awards", content: <>Test3</> },
+  { id: 3, title: "Certificates ", content: <>Test4</> },
+  { id: 4, title: "Seminars ", content: <>Test5</> },
 ];
-
-{
-  /* */
-}
