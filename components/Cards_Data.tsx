775a4a9356c73e9c74a7527696eb0f67871eac40
xcutiepatootie <mackycalabia@gmail.com>
2024-07-14 17:11:26 +0800

    feat: about section done

--- components/Cards_Data.tsx
+++ components/Cards_Data.tsx
@@ -7,10 +7,176 @@ type AboutCard = {
   content: ReactNode;
 };
 
+const TechnicalSkills = {
+  languages: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "Java",
+    "Python",
+    "C#",
+    "PHP",
+  ],
+  frameworks: ["React.js", "Next.js", "Django", "Spring Boot"],
+  libraries: [
+    "shadcn",
+    "material-ui",
+    "Tailwind CSS",
+    "FramerMotion",
+    "Aceternity UI",
+    "Bootstrap",
+    "Weka",
+  ],
+  tandps: ["Git", "Docker", "Google Cloud Platform", "Linux"],
+};
+
 export const about_cards: AboutCard[] = [
-  { id: 0, title: "Technical Skills", content: <>Test1</> },
-  { id: 1, title: "Soft Skills", content: <>Test2</> },
-  { id: 2, title: "Awards", content: <>Test3</> },
-  { id: 3, title: "Certificates ", content: <>Test4</> },
-  { id: 4, title: "Seminars ", content: <>Test5</> },
+  {
+    id: 0,
+    title: "Technical Skills",
+    content: (
+      <div className="w-auto">
+        <h1 className="text-2xl">Languages</h1>
+        <ul className="flex flex-wrap gap-2">
+          {TechnicalSkills.languages.map((language, index) => (
+            <li
+              className="w-auto rounded-md border border-slate-50 bg-[#739072] p-1 px-2 font-bold text-white"
+              key={index}
+            >
+              {language}
+            </li>
+          ))}
+        </ul>
+        <h1 className="text-2xl">Frameworks</h1>
+        <ul className="flex flex-wrap gap-2">
+          {TechnicalSkills.frameworks.map((framework, index) => (
+            <li
+              className="w-auto rounded-md border border-slate-50 bg-[#739072] p-1 px-2 font-bold text-white"
+              key={index}
+            >
+              {framework}
+            </li>
+          ))}
+        </ul>
+        <h1 className="text-2xl">Libraries</h1>
+        <ul className="flex flex-wrap gap-2">
+          {TechnicalSkills.libraries.map((library, index) => (
+            <li
+              className="w-auto rounded-md border border-slate-50 bg-[#739072] p-1 px-2 font-bold text-white"
+              key={index}
+            >
+              {library}
+            </li>
+          ))}
+        </ul>
+        <h1 className="text-2xl">Game Engine/s</h1>
+        <ul className="flex flex-wrap gap-2">
+          <li className="w-auto rounded-md border border-slate-50 bg-[#739072] p-1 px-2 font-bold text-white">
+            Unity
+          </li>
+        </ul>
+        <h1 className="text-2xl">Tools & Platforms</h1>
+        <ul className="flex flex-wrap gap-2">
+          {TechnicalSkills.tandps.map((tandp, index) => (
+            <li
+              className="w-auto rounded-md border border-slate-50 bg-[#739072] p-1 px-2 font-bold text-white"
+              key={index}
+            >
+              {tandp}
+            </li>
+          ))}
+        </ul>
+        <h1 className="text-2xl">Databases</h1>
+        <h2 className="text-xl">SQL</h2>
+        <ul className="flex flex-wrap gap-2">
+          <li className="w-auto rounded-md border border-slate-50 bg-[#739072] p-1 px-2 font-bold text-white">
+            MySQL
+          </li>
+          <li className="w-auto rounded-md border border-slate-50 bg-[#739072] p-1 px-2 font-bold text-white">
+            postgreSQL
+          </li>
+        </ul>
+        <h2 className="text-xl">NoSQL</h2>
+        <ul className="flex flex-wrap gap-2">
+          <li className="w-auto rounded-md border border-slate-50 bg-[#739072] p-1 px-2 font-bold text-white">
+            MongoDB
+          </li>
+        </ul>
+      </div>
+    ),
+  },
+  {
+    id: 1,
+    title: "Soft Skills",
+    content: (
+      <div>
+        <li>Project Management</li>
+        <li>Teamwork</li>
+        <li>Time Management</li>
+        <li>Leadership</li>
+        <li>Effective Communication</li>
+        <li>Critical Thinking</li>
+        <li>Adaptable</li>
+      </div>
+    ),
+  },
+  {
+    id: 2,
+    title: "Awards",
+    content: (
+      <div>
+        <li>
+          Cum Laude - Certificate of Recognition - Laguna State Polytechnic
+          University - San Pablo City Campus
+        </li>
+        <li>
+          Research Presenter - 9th National Research Conference on Information
+          Technology Education
+        </li>
+        <li>Research Presenter - 3rd place - CCS Student Research Congress</li>
+      </div>
+    ),
+  },
+  {
+    id: 3,
+    title: "Certificates ",
+    content: (
+      <div>
+        <li>MikroTik Certified Network Associate</li>
+        <li>Cisco - Introduction to Cybersecurity - Course Completion</li>
+        <li>Cisco - Cybersecurity Essentials - Course Completion</li>
+      </div>
+    ),
+  },
+  {
+    id: 4,
+    title: "Seminars ",
+    content: (
+      <div>
+        <li>
+          MikroTik Network Associate Bootcamp - Laguna State Polytechnic
+          University- San Pablo City Campus - June 2024
+        </li>
+        <li>
+          CCS Learn Fest 2023 - Exploring the Frontiers of Information
+          Technology and Computer Science - Laguna State Polytechnic University
+          - San Pablo City Campus - May 2023
+        </li>
+        <li>
+          13th EdukCircle International Convention on Engineering and Computer
+          Technology - UP Diliman - April 2023
+        </li>
+        <li>
+          The Essentials of Machine Learning - Laguna State Polytechnic
+          University San Pablo Campus College of Computer Studies - February
+          2021
+        </li>
+        <li>
+          Youth for Information Technology - UP System Information Technology
+          Foundation - December 2020
+        </li>
+      </div>
+    ),
+  },
 ];
--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,32 +1,81 @@
 "use client";
 
+import { TypeAnimation } from "react-type-animation";
 import { about_cards } from "../Cards_Data";
 import { BackgroundGradient } from "../ui/background-gradient";
 import { Vortex } from "../ui/vortex";
+import { animate, motion, useInView } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+
+const sequenceParagpraph =
+  "Hi there! While you are taking time to view my credentials. Once again, I am Macky. Here are my skills, awards, and certifications I earned during my school years." +
+  "\n\nI have always been fascinated by how computers operate. It is inevitable that technology will evolve quickly, and this means that I will need to pick up new skills day by day." +
+  "\n\nI am constantly working on upgrading my skills especially as a Web Developer. Learning new frameworks, libraries, and especially the building blocks of a web development. I also do standalone applications and native application for windows aside from web development.";
 
 const About = () => {
+  const [animationStarted, setAnimationStarted] = useState(false);
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      setAnimationStarted(true);
+    }
+  }, [isInView]);
+
   return (
-    <Vortex
-      backgroundColor="transparent"
-      baseHue={50}
-      particleCount={100}
-      rangeY={900}
-      className="flex h-full w-full flex-col items-center justify-center px-2 py-4 md:px-10"
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1, transition: { delay: 0.5 } }}
+      viewport={{ once: true }}
     >
-      <div className="flex flex-row space-x-4">
-        {about_cards.map((card) => (
-          <BackgroundGradient
-            key={card.id}
-            className="max-w-sm rounded-[22px] bg-slate-300 p-4 dark:bg-zinc-900 sm:p-10"
-          >
-            <div className="">
-              <h2>{card.title}</h2>
-              {card.content}
-            </div>
-          </BackgroundGradient>
-        ))}
-      </div>
-    </Vortex>
+      <Vortex
+        backgroundColor="transparent"
+        baseHue={50}
+        particleCount={100}
+        rangeY={900}
+        className="px-2 py-4 md:px-10"
+      >
+        <div className="grid grid-cols-3">
+          <div ref={ref} className="p-6 text-justify">
+            {animationStarted && (
+              <div>
+                <div>
+                  <TypeAnimation
+                    className="text-2xl font-light text-zinc-50"
+                    sequence={[sequenceParagpraph]}
+                    cursor={false}
+                    wrapper="span"
+                    speed={90}
+                    style={{
+                      whiteSpace: "pre-line",
+                      display: "block",
+                    }}
+                    repeat={0}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="col-span-2 flex flex-wrap items-center justify-center gap-4">
+            <BackgroundGradient
+              animate={false}
+              className="h-[700px] w-full overflow-scroll rounded-[22px] bg-slate-100 p-4 dark:bg-zinc-900 sm:p-10"
+            >
+              {about_cards.map((card) => (
+                <div key={card.id}>
+                  <div className="grid grid-cols-5 place-content-center">
+                    <h1 className="text-3xl">{card.title}</h1>
+                    <div className="col-span-4">{card.content}</div>
+                  </div>
+                  <hr className="my-4 border-t-2 border-slate-500" />
+                </div>
+              ))}
+            </BackgroundGradient>
+          </div>
+        </div>
+      </Vortex>
+    </motion.div>
   );
 };
 
