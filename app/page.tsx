8b5c72096eafcb09f677397bd0f4e3c93c64bbdb
xcutiepatootie <mackycalabia@gmail.com>
2024-07-16 20:29:14 +0800

    chore: removed unnecessary comments

--- app/page.tsx
+++ app/page.tsx
@@ -3,85 +3,30 @@ import { motion, useAnimation } from "framer-motion";
 import Home from "@/components/sections/Home";
 import About from "@/components/sections/About";
 import Projects from "@/components/sections/Projects";
+import Contact from "@/components/sections/Contact";
 
 export default function page() {
-  /* const scrollRef = useRef(null);
-
-  const controls = {
-    home: useAnimation(),
-    about: useAnimation(),
-    projects: useAnimation(),
-    contact: useAnimation(),
-  };
-
-  const sections = [
-    { id: "home", ref: useRef(null), animation: controls.home },
-    { id: "about", ref: useRef(null), animation: controls.about },
-    { id: "projects", ref: useRef(null), animation: controls.projects },
-    { id: "contact", ref: useRef(null), animation: controls.contact },
-  ];
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const section = sections.find(
-            (sec) => sec.ref.current === entry.target,
-          );
-          if (section) {
-            if (entry.isIntersecting) {
-              section.animation.start({ opacity: 1, scale: 1 });
-            } else {
-              section.animation.start({ opacity: 0.5, scale: 0.9 });
-            }
-          }
-        });
-      },
-      { threshold: 0.5 },
-    );
-
-    sections.forEach((section) => {
-      if (section.ref.current) {
-        observer.observe(section.ref.current);
-      }
-    });
-
-    return () => {
-      sections.forEach((section) => {
-        if (section.ref.current) {
-          observer.unobserve(section.ref.current);
-        }
-      });
-    };
-  }, [sections]); */
-
   return (
-    <div
-      /*  ref={scrollRef} */
-      className="h-screen w-screen snap-y snap-mandatory overflow-scroll scroll-smooth"
-    >
+    <div className="h-screen w-screen snap-y snap-mandatory overflow-scroll scroll-smooth">
       <Home />
 
       <div
-        /* ref={sections[1].ref} */
         className="flex h-screen w-screen snap-start flex-row items-center justify-center bg-[#739072]"
         id="about"
       >
         <About />
       </div>
       <div
-        /*   ref={sections[2].ref} */
-        className="flex h-screen w-screen snap-start flex-row py-20 justify-center bg-[#4F6F52]"
+        className="flex h-screen w-screen snap-start flex-row justify-center bg-[#4F6F52] py-20"
         id="projects"
       >
         <Projects />
       </div>
       <div
-        /*   ref={sections[3].ref} */
-        className="flex h-screen w-screen snap-start flex-row items-center justify-center bg-[#3A4D39]"
+        className="flex h-screen w-screen snap-start flex-row items-center justify-center bg-[#3A4D39] p-12"
         id="contact"
       >
-        Contacts
+        <Contact />
       </div>
     </div>
   );
